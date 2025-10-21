158804f57a92200f3c7ce462d096a5eab1624c08
Marcoxjocx02 <mvillanuevat@bsginstitute.com>
2025-10-21 11:39:01 -0500

    Fix: Validadores para aceptar null en algunos campos

--- BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ProgramaGeneralProblemaFactorDetalleDTO.cs
+++ BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ProgramaGeneralProblemaFactorDetalleDTO.cs
@@ -9,7 +9,7 @@ namespace BSI.Integra.Aplicacion.DTO.Modelos.IntegraDB
     public class ProgramaGeneralProblemaFactorDetalleDTO
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
         public string Titulo { get; set; }
 
     }
--- BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ProgramaGeneralProblemaFactorSolucionDTO.cs
+++ BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ProgramaGeneralProblemaFactorSolucionDTO.cs
@@ -9,8 +9,8 @@ namespace BSI.Integra.Aplicacion.DTO.SCode.Modelos.IntegraDB
     public class ProgramaGeneralProblemaFactorSolucionDTO
     {
         public int Id { get; set; }
-        public string Descripcion { get; set; }
-        public string Titulo { get; set; }
-        public string SubTitulo { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Titulo { get; set; }
+        public string? SubTitulo { get; set; }
     }
 }
