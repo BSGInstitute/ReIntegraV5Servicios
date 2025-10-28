162dad1fa9bbf5a3e6a85d7ef05c38ee19b1ffaa
ChristopherSParis <ctumir@bsginstitute.com>
2025-10-28 12:09:47 -0500

    feat: endpoint obtener programa general argumento motivacion por programa general

--- BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/Planificacion/ProgramaGeneralArgumentoDTO.cs
+++ BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/Planificacion/ProgramaGeneralArgumentoDTO.cs
@@ -45,41 +45,20 @@
         public int IdProgramaGeneralMotivacion { get; set; }
         public string NombreMotivacion { get; set; }
     }
-    //public class ProgramaGeneralArgumentoDTO
-    //{
-    //    public int Id { get; set; }
-    //    public int IdPgeneral { get; set; }
-    //    public string Nombre { get; set; }
-    //    public string Descripcion { get; set; }
-    //    public bool EsVisibleAgenda { get; set; }
-    //    public bool Estado { get; set; }
-    //}
-    //public class ProgramaGeneralArgumentoModalidadDTO
-    //{
-    //    public int Id { get; set; }
-    //    public int IdProgramaGeneralArgumento { get; set; }
-    //    public int IdModalidadCurso { get; set; }
-    //    public string Nombre { get; set; }
-    //    public bool Estado { get; set; }
-    //}
-    //public class ProgramaGeneralArgumentoDetalleDTO
-    //{
-    //    public int Id { get; set; }
-    //    public int IdProgramaGeneralArgumento { get; set; }
-    //    public string Detalle { get; set; }
-    //    public string InstrucionPieDetalle { get; set; }
-    //    public bool Estado { get; set; }
-    //}
-    //public class ProgramaGeneralArgumentoDetalleMotivacionDTO
-    //{
-    //    public int Id { get; set; }
-    //    public int IdProgramaGeneralArgumentoDetalle { get; set; }
-    //    public int IdProgramaGeneralMotivacion { get; set; }
-    //    public string NombreMotivacion { get; set; }
-    //    public bool Estado { get; set; }
-    //}
-
-
+    public class ArgumentoMotivacionProgramaGeneralDTO
+    {
+        public List<ArgumentoMotivacionEstructuraDTO> GarantiaDePrograma { get; set; }
+        public List<ArgumentoMotivacionEstructuraDTO> EstruturaCurricular { get; set; }
+        public List<ArgumentoMotivacionEstructuraDTO> DemostracionDeValor { get; set; }
+        public List<ArgumentoMotivacionEstructuraDTO> AspectosDiferenciadores { get; set; }
+        public List<ArgumentoMotivacionEstructuraDTO> ArgumentosDePerdidaPotencial { get; set; }
+    }
+    public class ArgumentoMotivacionEstructuraDTO
+    {
+        public ProgramaGeneralArgumentoDTO Argumento { get; set; }
+        public List<ProgramaGeneralArgumentoDetalleDTO> ArgumentoDetalle { get; set; }
+        public List<ProgramaGeneralArgumentoModalidadDTO> Modalidades { get; set; }
+    }
     public class ProgramaGeneralArgumentoMotivacionDTO
     {
         public int Id { get; set; }
