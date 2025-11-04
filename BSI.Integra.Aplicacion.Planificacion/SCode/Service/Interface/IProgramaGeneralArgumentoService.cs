3a77517c6dad85e75a51fa7583ca0290f0ecb6fc
BSG ti <ccrispin@bsginstitute.com>
2025-11-03 19:36:52 -0500

    avance

--- BSI.Integra.Aplicacion.Planificacion/SCode/Service/Interface/IProgramaGeneralArgumentoService.cs
+++ BSI.Integra.Aplicacion.Planificacion/SCode/Service/Interface/IProgramaGeneralArgumentoService.cs
@@ -22,7 +22,7 @@ namespace BSI.Integra.Aplicacion.Planificacion.Service.Interface
         IEnumerable<ProgramaGeneralArgumentoMotivacionDTO> ObtenerMotivaciones(int IdPGeneral);
         bool Eliminar(int id, string usuario);
         bool InsertarArgumentoMotivacionSeleccion(ProgramaArgumentoMotivacionSeleccionDTO data, string usuario);
-        object InsertarArgumentoMotivacionSeleccion(ProgramaArgumentoMotivacionSeleccionDTO data);
+        //object InsertarArgumentoMotivacionSeleccion(ProgramaArgumentoMotivacionSeleccionDTO data);
         Task<List<ProgramaGeneralArgumentoDTO>> ObtenerArgumentoMotivacion();
         Task<List<ConfiguracionProblemaJerarquicaDTO>> ObtenerProblemaCliente();
     }
