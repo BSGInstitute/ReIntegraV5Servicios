a733928f3d78ce403a5634b1d92e28d7c28bad07
Jose Raul Vega Tejada <jvegat@bsginstitute.com>
2025-12-22 19:15:27 -0500

    Se implementó ParametrosEnviarMensajePlaDTO y el método EnviarMensajeCorreoPla en GmailCorreoService para soportar el envío y validación de correos dirigidos a proveedores (docentes), agregando también la función ValidarEmailProveedor en AlumnoRepository.

--- BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ParametrosEnviarMensajeDTO.cs
+++ BSI.Integra.Aplicacion.DTO/SCode/Modelos/IntegraDB/ParametrosEnviarMensajeDTO.cs
@@ -17,6 +17,21 @@ namespace BSI.Integra.Aplicacion.DTO.Modelos.IntegraDB
         public string? DestinatarioBcc { get; set; }
         public string? GrupoEmail { get; set; }
     }
+    public class ParametrosEnviarMensajePlaDTO
+    {
+        public IList<IFormFile>? Files { get; set; }
+        public int? IdActividadDetalle { get; set; }
+        public int? IdCentroCosto { get; set; }
+        public int? IdGestionContacto { get; set; }
+        public int? IdClasificacionPersona { get; set; }
+        public string? Remitente { get; set; }
+        public string? Destinatario { get; set; }
+        public string? Asunto { get; set; }
+        public string Mensaje { get; set; }
+        public string? DestinatarioCc { get; set; }
+        public string? DestinatarioBcc { get; set; }
+        public string? GrupoEmail { get; set; }
+    }
     public class PerfilProfesionalDTO
     {
         public int? IdAFormacion { get; set; }
