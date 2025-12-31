ab0b3e4fabd83eddaf4c55b4ae4f63e75544f73e
ChristopherSParis <ctumir@bsginstitute.com>
2025-12-31 10:21:57 -0500

    feat: Implementacion Proyecto pruebas unitarias

---
+++ BSI.Integra.PruebasUnitarias/Test1.cs
@@ -0,0 +1,13 @@
+﻿namespace BSI.Integra.PruebasUnitarias
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var resultado = 2 + 3;
+            Assert.AreEqual(5, resultado);
+        }
+    }
+}
