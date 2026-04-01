ada580db056a665a89cf36db85a6d83cc6bfb72c
ChristopherSParis <ctumir@bsginstitute.com>
2026-04-01 13:00:36 -0500

    feat: Mapeo solicitado por jose vega

---
+++ BSI.Integra.Persistencia/Modelos/IntegraDB/TPagoEstado.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BSI.Integra.Persistencia.Modelos.IntegraDB
+{
+    /// <summary>
+    /// Almacena los estados del ciclo de pago (Solicitado PO, Observado FINA, Pendiente, Pagado FINANZ)
+    /// </summary>
+    public partial class TPagoEstado
+    {
+        public TPagoEstado()
+        {
+            TGestionPagos = new HashSet<TGestionPago>();
+        }
+
+        /// <summary>
+        /// Llave primaria de la tabla
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nombre del estado de pago
+        /// </summary>
+        public string Nombre { get; set; } = null!;
+        /// <summary>
+        /// Estado del registro (1: activo, 0: eliminado)
+        /// </summary>
+        public bool Estado { get; set; }
+        /// <summary>
+        /// Usuario creacion
+        /// </summary>
+        public string UsuarioCreacion { get; set; } = null!;
+        /// <summary>
+        /// Usuario modificacion
+        /// </summary>
+        public string UsuarioModificacion { get; set; } = null!;
+        /// <summary>
+        /// Fecha creacion
+        /// </summary>
+        public DateTime FechaCreacion { get; set; }
+        /// <summary>
+        /// Fecha modificacion
+        /// </summary>
+        public DateTime FechaModificacion { get; set; }
+        /// <summary>
+        /// RowVersion
+        /// </summary>
+        public byte[] RowVersion { get; set; } = null!;
+
+        public virtual ICollection<TGestionPago> TGestionPagos { get; set; }
+    }
+}
